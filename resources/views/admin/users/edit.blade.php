30e1adac39ef32dcf6276e9c431ff2ddb982e5fa
Jaco Grinwis <jacogrinwis@gmail.com>
2022-12-22 15:26:26 +0100

    differnt things

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -4,11 +4,6 @@
         <x-slot name="title">Edit User</x-slot>
     </x-slot>
 
-    {{-- <form action="{{ route('admin.users.update', $user) }}" method="POST">
-        @method('PUT')
-        <x-admin.users.form :user="$user" :roles="$roles" />
-    </form> --}}
-
     <div class="max-w-7xl mx-auto space-y-6">
         <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
             <div class="max-w-xl">
